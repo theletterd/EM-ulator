47739d4c07f08ad3bdb6784de45cc4146667702a
Duncan Cook <duncan.cook@theletterd.co.uk>
2023-11-08 09:46:26 -0800

    random ticket/game generation

--- em_ulator/blueprints/home/views.py
+++ em_ulator/blueprints/home/views.py
@@ -1,8 +1,17 @@
+import random
+import string
+
 from flask import Blueprint
 from flask import render_template
-import random
+from flask import redirect
+
+from em_ulator.models import Game
+from em_ulator.models import Project
+from em_ulator.models import Ticket
+
 
 home_app = Blueprint('home', __name__)
+
 
 @home_app.route("/")
 def index():
@@ -10,15 +19,41 @@ def index():
     fake_game_numbers = sorted(random.sample(range(100), 10))
     return render_template("index.html", games=fake_game_numbers)
 
-@home_app.route("/game/<int:game_number>/")
-def game(game_number):
+@home_app.route("/game/<int:game_id>/")
+def game(game_id):
+    # ok so what we want to do now is to load up all tickets  associated
+    # with this game, through projects.
+    # so let's jank this bad boy up
+    if not Game.exists(game_id):
+        return "no such game, dawg"
 
-    # 404 if no such game
-    return render_template("game.html", number=random.randint(0, 100))
+    tickets = Game.get_all_tickets(game_id)
+    return render_template("game.html", tickets=tickets, game_id=game_id)
 
 
 @home_app.route("/game/create")
 def create_game():
-    from em_ulator.models import Game
-    Game.create_new_game()
-    return "poops"
+    game = Game.create_new_game()
+    from em_ulator import db
+
+    random_project_name = ''.join(random.sample(string.ascii_uppercase, random.randrange(3, 8)))
+
+    project = Project(
+        name=random_project_name,
+        ticket_offset=random.randrange(1000),
+        game_id=game.id
+    )
+    db.session.add(project)
+    db.session.commit()
+
+    for i in range(10):
+        Ticket.new_ticket(project, f"new ticket yo: {i}", "stuffff")
+
+    # then create a project
+    # then create like 20 tickets in the project
+    # which are all in "TODO"
+    # Then redirect to game view which shows the tickets.
+
+    # we are also going to need a ticket title generator, and a ticket description generator
+    redirect_url = f'/game/{game.id}'
+    return redirect(redirect_url)
--- em_ulator/models.py
+++ em_ulator/models.py
@@ -1,4 +1,7 @@
+import random
+
 from em_ulator import db
+
 
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,6 +12,22 @@ class Game(db.Model):
         game = Game()
         db.session.add(game)
         db.session.commit()
+        return game
+
+
+    def exists(game_id):
+        game = Game.query.filter_by(id=game_id).first()
+        return bool(game)
+
+    def get_all_tickets(game_id):
+        # probably a better way to do this.
+        projects = Project.query.filter_by(game_id=game_id).all()
+        project_ids = [project.id for project in projects]
+        tickets = Ticket.query.filter(
+            Ticket.project_id.in_(project_ids)
+        ).order_by(Ticket.id.asc()).all()
+
+        return tickets
 
 
     def tick(self):
@@ -27,7 +46,9 @@ class Game(db.Model):
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
-
+    ticket_offset = db.Column(db.Integer, nullable=False)
+    game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
+    game = db.relationship('Game')
 
 
 
@@ -35,11 +56,31 @@ class Ticket(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
     project = db.relationship('Project')
+    title = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.String(2048), nullable=False)
 
     # e.g., JIRA-1234
     key = db.Column(db.String(120), unique=True, nullable=False)
     #state_id = db.Column(db.Integer, db.ForeignKey('ticketstate.id'), nullable=False)
     #state = db.relationship('TicketState')
+
+    @staticmethod
+    def new_ticket(project, title, description):
+        num_existing_tickets = Ticket.query.filter_by(project_id=project.id).count()
+        # how many tickets are already in this project?
+        # let's just say 100 for now
+        # we can do counting later.
+        ticket_id = num_existing_tickets + 1 + project.ticket_offset
+        key = f"{project.name}-{ticket_id}"
+        ticket = Ticket(
+            project_id=project.id,
+            key=key,
+            title=title,
+            description=description
+        )
+
+        db.session.add(ticket)
+        db.session.commit()
 
 
 
